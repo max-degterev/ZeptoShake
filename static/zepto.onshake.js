1528aa91694318fb60b508c05e7b48befb5c31cb
Max Degterev <me@maxdegterev.name>
2014-04-20 16:06:13 +0100

    some changes

--- static/zepto.onshake.js
+++ static/zepto.onshake.js
@@ -1,57 +1,55 @@
-/* Author:
-    Max Degterev @suprMax
-	
-	$.onshake(callb, sens);
-	
-	based on  http://www.jeffreyharrell.com/blog/2010/11/creating-a-shake-event-in-mobile-safari/
-*/
-
 ;(function($) {
-	if (typeof window.DeviceMotionEvent !== 'undefined') {
-		$.onshake = function(callb, sens) {
-			// Shake sensitivity (a lower number is more sensitive)
-			var sensitivity = sens || 20,
-				checkDelay = 150,
-				callbackDelay = 2500;
+  var $window = $(window);
 
-			// Position variables
-			var x1 = 0,
-				y1 = 0,
-				z1 = 0,
-				x2 = 0,
-				y2 = 0,
-				z2 = 0;
-				
-			var checkDeviceMotion = function() {
-				var change = Math.abs((x1 - x2) + (y1 - y2) + (z1 - z2));
-				
-				// Update new position
-				x2 = x1;
-				y2 = y1;
-				z2 = z1;
+  if (typeof window.DeviceMotionEvent !== 'undefined') {
+    $.onshake = function(callback, _sensitivity) {
 
-				if (change > sensitivity) {
-					callb.call(window);
-					setTimeout(checkDeviceMotion, callbackDelay);
-				}
-				else {
-					setTimeout(checkDeviceMotion, checkDelay);
-				}	
-			};
+      // Shake sensitivity (a lower number is more sensitive)
+      var sensitivity = typeof _sensitivity === 'number' ? _sensitivity : 20,
+        checkDelay = 100,
+        callbackDelay = 2500; // protection from having 9999 calls at once
 
-			// Listen to motion events and update the position
-			window.addEventListener('devicemotion', function (e) {
-				x1 = e.accelerationIncludingGravity.x;
-				y1 = e.accelerationIncludingGravity.y;
-				z1 = e.accelerationIncludingGravity.z;
-			}, false);
+      // Position variables
+      var x1 = 0,
+          y1 = 0,
+          z1 = 0,
+          x2 = 0,
+          y2 = 0,
+          z2 = 0;
 
-			// Periodically check the position and fire
-			// if the change is greater than the sensitivity
-			checkDeviceMotion();
-		};	
-	}
-	else {
-		$.onshake = function() {};
-	}
+      var checkDeviceMotion = function() {
+        var change = Math.abs((x1 - x2) + (y1 - y2) + (z1 - z2));
+
+        // Update new position
+        x2 = x1;
+        y2 = y1;
+        z2 = z1;
+
+        if (change > sensitivity) {
+          callback.call(window, change);
+          $window.trigger('shake');
+          setTimeout(checkDeviceMotion, callbackDelay);
+        }
+        else {
+          setTimeout(checkDeviceMotion, checkDelay);
+        }
+      };
+
+      var storeMotionData = function(e) {
+        x1 = e.accelerationIncludingGravity.x;
+        y1 = e.accelerationIncludingGravity.y;
+        z1 = e.accelerationIncludingGravity.z;
+      };
+
+      // Listen to motion events and update the position
+      $window.on('devicemotion', storeMotionData);
+
+      // Periodically check the position and fire
+      // if the change is greater than the sensitivity
+      checkDeviceMotion();
+    };
+  }
+  else {
+    $.onshake = function() {};
+  }
 })(Zepto);
